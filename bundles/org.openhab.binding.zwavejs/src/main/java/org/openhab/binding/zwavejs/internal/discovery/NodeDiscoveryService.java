f126e0850cc8dda2bd918fd2d94ff10717c88200
Leo Siepel <leosiepel@gmail.com>
2024-12-31 09:53:29 +0100

    Fix NPE
    Signed-off-by: Leo Siepel <leosiepel@gmail.com>

--- bundles/org.openhab.binding.zwavejs/src/main/java/org/openhab/binding/zwavejs/internal/discovery/NodeDiscoveryService.java
+++ bundles/org.openhab.binding.zwavejs/src/main/java/org/openhab/binding/zwavejs/internal/discovery/NodeDiscoveryService.java
@@ -122,7 +122,8 @@ public class NodeDiscoveryService extends AbstractThingHandlerDiscoveryService<Z
             properties.put(PROPERTY_NODE_IS_LISTENING, node.isListening);
             properties.put(PROPERTY_NODE_IS_ROUTING, node.isRouting);
             properties.put(PROPERTY_NODE_IS_SECURE, node.isSecure);
-            properties.put(PROPERTY_NODE_MANUFACTURER, node.deviceConfig.manufacturer);
+            properties.put(PROPERTY_NODE_MANUFACTURER,
+                    node.deviceConfig != null ? node.deviceConfig.manufacturer : "Unknown");
             properties.put(PROPERTY_NODE_PRODUCT_ID, node.productId);
             properties.put(PROPERTY_NODE_PRODUCT_TYPE, node.productType);
             properties.put(PROPERTY_NODE_LASTSEEN, node.lastSeen);
